2faed8287971b1199940507b0d5327e3b64b5947
Niki Rahmadi Wiharto <nikirahmadiwiharto@gmail.com>
2019-12-05 22:27:38 +0700

    finish materi 05

---
+++ app/src/main/java/com/niki/dicodingjetpackpro/MainActivity.java
@@ -0,0 +1,4 @@
+package com.niki.dicodingjetpackpro;
+
+public class MainActivity {
+}
---
+++ app/src/main/java/com/niki/dicodingjetpackpro/MainViewModel.java
@@ -0,0 +1,20 @@
+package com.niki.dicodingjetpackpro;
+
+public class MainViewModel {
+    private CuboidModel cuboidModel;
+    MainViewModel(CuboidModel cuboidModel) {
+        this.cuboidModel = cuboidModel;
+    }
+    void save(double l, double w, double h) {
+        cuboidModel.save(l, w, h);
+    }
+    double getCircumference() {
+        return cuboidModel.getCircumference();
+    }
+    double getSurfaceArea() {
+        return cuboidModel.getSurfaceArea();
+    }
+    double getVolume() {
+        return cuboidModel.getVolume();
+    }
+}
